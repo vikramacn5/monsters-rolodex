50b3e38297745a953750bcd810d4e544f4c109af
vikramacn5 <vikram.cn5@gmail.com>
2021-12-10 15:58:44 +0530

    Till setstate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        {
+          name: "Frankinstein",
+          id: "asc1",
+        },
+        {
+          name: "Zombie",
+          id: "asc2",
+        },
+        {
+          name: "MarkIV",
+          id: "asc3",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
+
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <header className="App-header">
+  //         <img src={logo} className="App-logo" alt="logo" />
+  //         {[
+  //           <p>{this.state.name}</p>,
+  //           <p>{this.state.name}</p>,
+  //           <p>{this.state.name}</p>,
+  //         ]}
+  //         <button
+  //           onClick={() => {
+  //             this.setState({ name: "Hello I am markIV" });
+  //           }}
+  //         >
+  //           Click me
+  //         </button>
+  //       </header>
+  //     </div>
+  //   );
+  // }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
