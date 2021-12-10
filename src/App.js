a4716f70319adf4b2b7cf7f91a202f5e2fb4ab9a
vikramacn5 <vikram.cn5@gmail.com>
2021-12-10 19:19:23 +0530

    added robo images into cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
 
 class App extends Component {
   constructor() {
@@ -18,9 +19,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
